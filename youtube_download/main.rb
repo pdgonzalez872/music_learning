3d0b2e78e6c9f99ffa8a0340f8f912b3b88fe4da
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-07-08 14:57:31 -0500

    Remove unnecessary cd

--- youtube_download/main.rb
+++ youtube_download/main.rb
@@ -7,7 +7,7 @@ my_pwd = "/Users/paulogonzalez1/Desktop/dev/youtube_video_downloads"
 file_names = File.read("urls.txt").split("\n")
 
 file_names.each do |f|
-  system("cd #{my_pwd}; youtube-dl --extract-audio --audio-format mp3 #{f}")
+  system("youtube-dl --extract-audio --audio-format mp3 #{f}")
 end
 
 puts "end"
