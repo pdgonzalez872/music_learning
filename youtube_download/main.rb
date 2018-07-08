3f85ce9da49f77163b467045ade98fae8b67cf60
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-07-08 14:02:40 -0500

    add youtube download

---
+++ youtube_download/main.rb
@@ -0,0 +1,13 @@
+# youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=aLnSO6FQpHs &&
+#
+puts "start"
+
+my_pwd = "/Users/paulogonzalez1/Desktop/dev/youtube_video_downloads"
+
+file_names = File.read("urls.txt").split("\n")
+
+file_names.each do |f|
+  system("cd #{my_pwd}; youtube-dl --extract-audio --audio-format mp3 #{f}")
+end
+
+puts "end"
